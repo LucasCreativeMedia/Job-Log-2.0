c53b25ed14c523857363a7d7077c3d55f2c74374
Tim <72915464+LucasCreativeMedia@users.noreply.github.com>
2022-01-20 13:30:10 -0800

    Update config_db.php

--- config_db.php
+++ config_db.php
@@ -1,8 +1,8 @@
 <?php
-    //$servername = "localhost";
-    $username = "tlucas44";
+    //$servername = "localhost:3306";
+    $username = "lucascre_talucas";
     $password = "Jesus2021";
-    $dbname = "tl_job_log";
+    $dbname = "lucascre_projecttrack";
 
-    $db = new PDO('mysql:host=localhost;dbname=' . $dbname . ';charset=utf8', $username, $password);
+    $db = new PDO('mysql:host=localhost:3306;dbname=' . $dbname . ';charset=utf8', $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
